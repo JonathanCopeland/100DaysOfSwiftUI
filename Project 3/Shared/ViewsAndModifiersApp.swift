1b074a8dce847ac25b584592b692fe350da00cbb
Jonathan Copeland <jonathan.mcopeland@icloud.com>
2020-12-30 16:43:07 +0200

    Upload: Project 3
    Uploading previously completed projects from the #100DaysOfSwiftUI Hacking with Swift challenge

---
+++ Project 3/Shared/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  Shared
+//
+//  Created by Jonathan Copeland on 2020/12/27.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

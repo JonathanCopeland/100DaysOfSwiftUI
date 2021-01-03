a397fc1556dbe23579f43bb55cb5061e2dcf966d
Jonathan Copeland <jonathan.mcopeland@icloud.com>
2021-01-03 14:11:06 +0200

    Completed part 2 of Project 4

---
+++ BetterRest/Shared/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  Shared
+//
+//  Created by Jonathan Copeland on 03/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

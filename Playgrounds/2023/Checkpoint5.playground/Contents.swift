e411da31f27328febd5296f33f30956bce700673
Jonathan Copeland <10074185+JonathanCopeland@users.noreply.github.com>
2023-02-05 20:45:14 +0000

    Day9

--- Playgrounds/2023/Checkpoint5.playground/Contents.swift
+++ Playgrounds/2023/Checkpoint5.playground/Contents.swift
@@ -1,3 +1,31 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+// Instructions
+    // Filter out any numbers that are even
+    // Sort the array in ascending order
+    // Map them to strings in the format “7 is a lucky number”
+    // Print the resulting array, one item per line
+
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+let filterArray = luckyNumbers.filter {!$0.isMultiple(of: 2)}
+print(filterArray)
+
+print()
+
+let sortArray = filterArray.sorted()
+print(sortArray)
+
+print()
+
+let mapArray = sortArray.map {
+    var x = "\($0) is a lucky number"
+    return x
+}
+print(mapArray)
+
+print()
+
+for printArray in mapArray {
+    print("\(printArray)")
+}

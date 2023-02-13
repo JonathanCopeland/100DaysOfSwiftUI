5d91fc90908c2c0cbd21d4952d587e4f6191b1d0
Jonathan Copeland <10074185+JonathanCopeland@users.noreply.github.com>
2023-02-13 20:31:41 +0000

    Project 1

---
+++ SwiftUI/Projects/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jonathan Copeland on 13/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

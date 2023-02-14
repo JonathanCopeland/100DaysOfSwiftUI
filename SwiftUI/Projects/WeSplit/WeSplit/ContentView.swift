366d3806d61d157bf008d836e75313fd17aa0829
Jonathan Copeland <10074185+JonathanCopeland@users.noreply.github.com>
2023-02-14 10:27:00 +0000

    Project 1

--- SwiftUI/Projects/WeSplit/WeSplit/ContentView.swift
+++ SwiftUI/Projects/WeSplit/WeSplit/ContentView.swift
@@ -43,33 +43,25 @@ struct ContentView: View {
                         .focused($amountIsFocused)
                     
                     Picker("Number of people", selection: $numberOfPeople) {
-                        ForEach(2..<100) {
+                        ForEach(2..<11) {
                             Text("\($0) people")
                         }
                     }
-                }
-                
-                Section {
+                    
                     Picker("Tip percentage", selection: $tipPercentage) {
                         ForEach(tipPercentages, id: \.self) {
                             Text($0, format: .percent)
                         }
                     }
-                    .pickerStyle(.segmented)
-                } header: {
-                    Text("How much tip do you want to leave?")
                 }
                 
                 Section {
-                    Text(total, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    
+                    Text("\(total, format: .currency(code: Locale.current.currencyCode ?? "USD")) (\(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD")) per person)")
+                    
+        
                 } header: {
                     Text("Total")
-                }
-                
-                Section {
-                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
-                } header: {
-                    Text("Amount per person")
                 }
                 
             }

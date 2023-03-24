3dc0873908bc7f9d17cb24a62d49df17ddcd9f5e
Jonathan Copeland <10074185+JonathanCopeland@users.noreply.github.com>
2023-03-24 09:20:40 +0000

    Project 7

--- SwiftUI/Projects/iExpense/iExpense/ContentView.swift
+++ SwiftUI/Projects/iExpense/iExpense/ContentView.swift
@@ -11,15 +11,46 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+    @StateObject var expenses = Expenses()
+    @State private var showingAddExpense = false
+
 
 
     var body: some View {
-        Button("Tap count: \(tapCount)") {
-            tapCount += 1
-            UserDefaults.standard.set(self.tapCount, forKey: "Tap")
+        
+        NavigationView {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
 
+                        Spacer()
+                        Text(item.amount, format: .currency(code: "USD"))
+                    }
+                }
+                .onDelete(perform: removeItems)
+
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button {
+                    showingAddExpense = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
+            }
         }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
     
 

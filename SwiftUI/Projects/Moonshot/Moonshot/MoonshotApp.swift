3c4ecd1dfda3cd4d8f8b8d3f4b4398b7d2045fe8
Jonathan Copeland <10074185+JonathanCopeland@users.noreply.github.com>
2023-04-01 13:30:44 +0100

    Project 8

---
+++ SwiftUI/Projects/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Jonathan Copeland on 01/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

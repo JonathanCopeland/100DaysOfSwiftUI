217ed76232542c745d51203042a15e8ca9c4f494
Jonathan Copeland <10074185+JonathanCopeland@users.noreply.github.com>
2023-04-04 08:09:35 +0100

    Project 8

---
+++ SwiftUI/Projects/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Jonathan Copeland on 02/04/2023.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
+

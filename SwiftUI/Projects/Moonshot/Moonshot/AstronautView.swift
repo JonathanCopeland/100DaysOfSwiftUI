b84eddcfabf2788144fd7a0533c14a36285b60ee
Jonathan Copeland <10074185+JonathanCopeland@users.noreply.github.com>
2023-04-06 08:35:53 +0100

    Project 8

---
+++ SwiftUI/Projects/Moonshot/Moonshot/AstronautView.swift
@@ -0,0 +1,45 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Jonathan Copeland on 06/04/2023.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                VStack (alignment: .leading) {
+
+
+                    Text(astronaut.description)
+                }
+                .padding()
+
+                
+
+            }
+            
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+
+    static var previews: some View {
+        AstronautView(astronaut: astronauts["aldrin"]!)
+            .preferredColorScheme(.dark)
+    }
+}

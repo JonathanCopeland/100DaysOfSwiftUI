ec484523520e7124b6ed848f66e64f9ac4f6b911
Jonathan Copeland <10074185+JonathanCopeland@users.noreply.github.com>
2023-05-06 22:16:18 +0100

    Project 13

--- SwiftUI/Projects/Instafilter/Instafilter/ContentView.swift
+++ SwiftUI/Projects/Instafilter/Instafilter/ContentView.swift
@@ -8,26 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
+    @State private var image: Image?
+    @State private var showingImagePicker = false
 
     var body: some View {
-        Text("Hello, World!")
-            .frame(width: 300, height: 300)
-            .background(backgroundColor)
-            .onTapGesture {
-                showingConfirmation = true
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+
+            Button("Select Image") {
+               showingImagePicker = true
             }
-            .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-                Button("Red") { backgroundColor = .red }
-                Button("Green") { backgroundColor = .green }
-                Button("Blue") { backgroundColor = .blue }
-                Button("Cancel", role: .cancel) { }
-            } message: {
-                Text("Select a new color")
-            }
+        }
+        .sheet(isPresented: $showingImagePicker) {
+            ImagePicker()
+        }
     }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ SwiftUI/Projects/Instafilter/Instafilter/ImagePicker.swift
@@ -0,0 +1,25 @@
+//
+//  ImagePicker.swift
+//  Instafilter
+//
+//  Created by Jonathan Copeland on 06/05/2023.
+//
+
+import PhotosUI
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+
+        let picker = PHPickerViewController(configuration: config)
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+        
+    }
+    
+
+}

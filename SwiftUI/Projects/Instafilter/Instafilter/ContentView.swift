06776f7036c9d5678bd3801c3704edae162223cf
Jonathan Copeland <10074185+JonathanCopeland@users.noreply.github.com>
2023-05-07 11:06:22 +0100

    Project 13

--- SwiftUI/Projects/Instafilter/Instafilter/ContentView.swift
+++ SwiftUI/Projects/Instafilter/Instafilter/ContentView.swift
@@ -3,12 +3,15 @@
 //  Instafilter
 //
 //  Created by Jonathan Copeland on 05/05/2023.
-//
+//  See 20:00 in the video for summary https://www.hackingwithswift.com/books/ios-swiftui/using-coordinators-to-manage-swiftui-view-controllers
 
 import SwiftUI
 
+// This needs to be reset, but leave the helper files
+
 struct ContentView: View {
     @State private var image: Image?
+    @State private var inputImage: UIImage?
     @State private var showingImagePicker = false
 
     var body: some View {
@@ -20,10 +23,25 @@ struct ContentView: View {
             Button("Select Image") {
                showingImagePicker = true
             }
+            
+            Button("Save Image") {
+                guard let inputImage = inputImage else { return }
+
+                let imageSaver = ImageSaver()
+                imageSaver.writeToPhotoAlbum(image: inputImage)
+            }
+            
         }
         .sheet(isPresented: $showingImagePicker) {
-            ImagePicker()
+            ImagePicker(image: $inputImage)
         }
+        .onChange(of: inputImage) { _ in loadImage() }
+
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
     }
 }
 

58b75582f39db4018da2c6acf8cc160136498a2e
Jonathan Copeland <10074185+JonathanCopeland@users.noreply.github.com>
2023-05-08 18:49:11 +0100

    Project 13

---
+++ SwiftUI/Projects/BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Jonathan Copeland on 08/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- SwiftUI/Projects/Instafilter/Instafilter/ContentView.swift
+++ SwiftUI/Projects/Instafilter/Instafilter/ContentView.swift
@@ -12,6 +12,10 @@ import CoreImage.CIFilterBuiltins
 struct ContentView: View {
     @State private var image : Image?
     @State private var filterIntensity = 0.5
+    @State private var filterRadius = 0.5
+    @State private var filterScale = 0.5
+    
+    @State private var placeholderEmpty = true
     
     @State private var showingImagePicker = false
     @State private var inputImage: UIImage?
@@ -28,11 +32,15 @@ struct ContentView: View {
             VStack {
                 ZStack {
                     Rectangle()
-                        .fill(.secondary)
+                        .fill(.white)
+                        .cornerRadius(4)
+                    
+                    if(image == nil) {
+                        Text("Tap to select a picture")
+                            .foregroundColor(.blue)
+                            .font(.headline)
 
-                    Text("Tap to select a picture")
-                        .foregroundColor(.white)
-                        .font(.headline)
+                    }
 
                     image?
                         .resizable()
@@ -41,41 +49,99 @@ struct ContentView: View {
                 .onTapGesture {
                     showingImagePicker = true
                 }
-
-                HStack {
-                    Text("Intensity")
-                    Slider(value: $filterIntensity)
-                        .onChange(of: filterIntensity) { _ in
-                            applyProcessing()
+                
+                VStack {
+                                            
+                        if(currentFilter.inputKeys.contains(kCIInputIntensityKey)) {
+                            HStack {
+                                Text("Intensity")
+                                Slider(value: $filterIntensity)
+                                    .onChange(of: filterIntensity) { _ in
+                                        applyProcessing()
+                                        
+                                    }
+                            }
+ 
                         }
+                    
+                                             
+                        if(currentFilter.inputKeys.contains(kCIInputRadiusKey)) {
+                            HStack {
+                                Text("Radius")
+                                Slider(
+                                    value: $filterRadius,
+                                    in: 0...1
+                                )
+                                    .onChange(of: filterRadius) { _ in
+                                        applyProcessing()
+                                }
+                            }
+                        }
+                    
+                        if(currentFilter.inputKeys.contains(kCIInputScaleKey)) {
+                            HStack {
+                                Text("Scale")
+                                Slider(value: $filterScale)
+                                    .onChange(of: filterScale) { _ in
+                                        applyProcessing()
+                                }
+                            }
+                        }
+      
                 }
                 .padding(.vertical)
+
+                
+
+
+
 
                 HStack {
                     Button("Change Filter") {
                         showingFilterSheet = true
+                        
                     }
 
                     Spacer()
 
-                    Button("Save", action: save)
+                    Button("Save") {
+                        save()
+                    }
+                    .disabled(placeholderEmpty)
+                    
+                    
+                    
                 }
             }
             .padding([.horizontal, .bottom])
             .navigationTitle("Instafilter")
-            .onChange(of: inputImage) { _ in loadImage() }
+            .onChange(of: inputImage) {
+                _ in loadImage()
+                placeholderEmpty = false
+            }
             .sheet(isPresented: $showingImagePicker) {
                 ImagePicker(image: $inputImage)
             }
             .confirmationDialog("Select a filter", isPresented: $showingFilterSheet) {
-                Button("Crystallize") { setFilter(CIFilter.crystallize()) }
-                Button("Edges") { setFilter(CIFilter.edges()) }
-                Button("Gaussian Blur") { setFilter(CIFilter.gaussianBlur()) }
-                Button("Pixellate") { setFilter(CIFilter.pixellate()) }
-                Button("Sepia Tone") { setFilter(CIFilter.sepiaTone()) }
-                Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask()) }
-                Button("Vignette") { setFilter(CIFilter.vignette()) }
-                Button("Cancel", role: .cancel) { }
+                Group{
+                    Button("Crystallize") { setFilter(CIFilter.crystallize()) }
+                    Button("Gaussian Blur") { setFilter(CIFilter.gaussianBlur()) }
+                    Button("Edges") { setFilter(CIFilter.edges()) }
+                    Button("Pixellate") { setFilter(CIFilter.pixellate()) }
+                    Button("Sepia Tone") { setFilter(CIFilter.sepiaTone()) }
+                    Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask()) }
+                    Button("Vignette") { setFilter(CIFilter.vignette()) }
+                    Button("Bloom") { setFilter(CIFilter.bloom()) }
+                    Button("Comic") { setFilter(CIFilter.comicEffect()) }
+                    Button("xRay") { setFilter(CIFilter.xRay()) }
+
+                }
+                
+                Group {
+                    Button("Noir") { setFilter(CIFilter.photoEffectNoir()) }
+                    Button("Pointillize") { setFilter(CIFilter.pointillize()) }
+                    Button("Cancel", role: .cancel) { }
+                }
             }
         }
         
@@ -110,8 +176,8 @@ struct ContentView: View {
         let inputKeys = currentFilter.inputKeys
 
         if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey) }
-        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterRadius * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterScale * 10, forKey: kCIInputScaleKey) }
         
         guard let outputImage = currentFilter.outputImage else { return }
 

f3e2fb6aa8a077da1941ecada180b2c88a492f2f
Jonathan Copeland <10074185+JonathanCopeland@users.noreply.github.com>
2023-05-11 17:59:05 +0100

    Project 15

---
+++ SwiftUI/Projects/AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Jonathan Copeland on 11/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

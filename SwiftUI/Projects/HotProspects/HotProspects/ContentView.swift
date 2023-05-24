fde70459fd0762d0534409387b366f50a33209e9
Jonathan Copeland <10074185+JonathanCopeland@users.noreply.github.com>
2023-05-24 08:11:56 +0100

    Project 16

---
+++ SwiftUI/Projects/HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Jonathan Copeland on 24/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var selectedTab = "One"
+
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            Text("Tab 1")
+                .onTapGesture {
+                    selectedTab = "Two"
+                }
+                .tabItem {
+                    Label("One", systemImage: "star")
+                }
+                .tag("One")
+
+
+            Text("Tab 2")
+                .tabItem {
+                    Label("Two", systemImage: "circle")
+                }
+                .tag("Two")
+
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI/Projects/HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Jonathan Copeland on 24/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

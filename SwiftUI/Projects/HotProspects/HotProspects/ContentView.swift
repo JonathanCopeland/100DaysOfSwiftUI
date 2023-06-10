955d9c4c25338ce430e28a3d534e49c48dc1dd85
Jonathan Copeland <10074185+JonathanCopeland@users.noreply.github.com>
2023-06-10 18:31:53 +0100

    Day 80

--- SwiftUI/Projects/HotProspects/HotProspects/ContentView.swift
+++ SwiftUI/Projects/HotProspects/HotProspects/ContentView.swift
@@ -8,31 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var selectedTab = "One"
 
-    
+    @State private var output = ""
+
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Text("Tab 1")
-                .onTapGesture {
-                    selectedTab = "Two"
-                }
-                .tabItem {
-                    Label("One", systemImage: "star")
-                }
-                .tag("One")
-
-
-            Text("Tab 2")
-                .tabItem {
-                    Label("Two", systemImage: "circle")
-                }
-                .tag("Two")
-
-            
-        }
+        Text(output)
+            .task {
+                await fetchReadings()
+            }
     }
+
+    func fetchReadings() async {
+        let fetchTask = Task { () -> String in
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "Found \(readings.count) readings"
+        }
+        
+        let result = await fetchTask.result
+        
+        switch result {
+            case .success(let str):
+                output = str
+            case .failure(let error):
+                output = "Error: \(error.localizedDescription)"
+        }
+
+    }
+
+    
 }
 
 struct ContentView_Previews: PreviewProvider {

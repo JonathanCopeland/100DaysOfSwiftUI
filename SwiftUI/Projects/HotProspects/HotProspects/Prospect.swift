eac21617c7ea11b87974ba7041452dd62f400992
Jonathan Copeland <10074185+JonathanCopeland@users.noreply.github.com>
2023-06-12 08:59:51 +0100

    Day 82

---
+++ SwiftUI/Projects/HotProspects/HotProspects/Prospect.swift
@@ -0,0 +1,18 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Jonathan Copeland on 12/06/2023.
+//
+
+import SwiftUI
+
+struct Prospect: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Prospect()
+}
---
+++ SwiftUI/Projects/HotProspects/HotProspects/ProspectsView.swift
@@ -0,0 +1,18 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Jonathan Copeland on 12/06/2023.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProspectsView()
+}

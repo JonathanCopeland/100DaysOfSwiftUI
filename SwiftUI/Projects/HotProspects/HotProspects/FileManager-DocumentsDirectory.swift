94a98247dbe2f62c2af91922ed543d642f3370d1
Jonathan Copeland <10074185+JonathanCopeland@users.noreply.github.com>
2023-06-17 10:12:39 +0100

    Project 16

---
+++ SwiftUI/Projects/HotProspects/HotProspects/FileManager-DocumentsDirectory.swift
@@ -0,0 +1,8 @@
+//
+//  FileManager-DocumentsDirectory.swift
+//  HotProspects
+//
+//  Created by Jonathan Copeland on 17/06/2023.
+//
+
+import Foundation
---
+++ SwiftUI/Projects/HotProspects/HotProspects/ImageSaver.swift
@@ -0,0 +1,8 @@
+//
+//  ImageSaver.swift
+//  HotProspects
+//
+//  Created by Jonathan Copeland on 17/06/2023.
+//
+
+import Foundation

b359910b723df895d1ce5e0e279563692d3e6406
Jonathan Copeland <10074185+JonathanCopeland@users.noreply.github.com>
2023-06-19 22:11:38 +0100

    Day 86

---
+++ SwiftUI/Projects/FlashZilla/FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaApp.swift
+//  FlashZilla
+//
+//  Created by Jonathan Copeland on 19/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

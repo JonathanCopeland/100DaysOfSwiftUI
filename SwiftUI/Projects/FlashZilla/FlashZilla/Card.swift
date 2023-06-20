7fe8eb1469b30a6b857d9f4923e6341b364b2c57
Jonathan Copeland <JonathanCopeland@users.noreply.github.com>
2023-06-20 08:21:36 +0100

    Day 88

---
+++ SwiftUI/Projects/FlashZilla/FlashZilla/Card.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "Who is the lead singer of the band Korn?", answer: "Jonathan David")
+}
--- SwiftUI/Projects/FlashZilla/FlashZilla/ContentView.swift
+++ SwiftUI/Projects/FlashZilla/FlashZilla/ContentView.swift
@@ -5,17 +5,42 @@
 //  Created by Jonathan Copeland on 19/06/2023.
 //
 
+
 import SwiftUI
 
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(x: 0, y: offset * 10)
+    }
+}
+
 struct ContentView: View {
+    
+    @State private var cards = [Card](repeating: Card.example, count: 10)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("background@2x")
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index]) {
+                            withAnimation {
+                                removeCard(at: index)
+                            }
+                        }
+                        .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
         }
-        .padding()
+    }
+    
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
     }
 }
 

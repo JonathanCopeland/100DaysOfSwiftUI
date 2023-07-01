399fb81ab3f4d287d894cfff79450c2da892c08a
Jonathan Copeland <10074185+JonathanCopeland@users.noreply.github.com>
2023-07-01 22:58:46 +0100

    Day 94

--- SwiftUI/Projects/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ SwiftUI/Projects/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -13,14 +13,17 @@ struct ContentView: View {
 
     var body: some View {
         GeometryReader { fullView in
-            ScrollView {
+            ScrollView(.vertical) {
                 ForEach(0..<50) { index in
                     GeometryReader { geo in
                         Text("Row #\(index)")
                             .font(.title)
                             .frame(maxWidth: .infinity)
-                            .background(colors[index % 7])
+                            .background(Color(hue: min(1, geo.frame(in: .global).minY / fullView.size.height), saturation: 1, brightness: 1))
                             .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .opacity(geo.frame(in: .global).minY / 200)
+                        
+
                     }
                     .frame(height: 40)
                 }
